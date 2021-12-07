cad4631745536bc407d607ea4910491e572e1701
Felipe S <rfelipe273@gmail.com>
2021-12-07 05:13:01 -0300

    ensure Transaction throws if amount is greater than 10000.

--- src/core/entity/Transaction.java
+++ src/core/entity/Transaction.java
@@ -3,13 +3,11 @@ package core.entity;
 import helpers.transaction.InvalidTransaction;
 
 public class Transaction {
-    private String id;
     private String accountID;
     private float amount;
     private CreditCard creditCard;
 
-    public Transaction(String id, String accountID, float amount) throws InvalidTransaction {
-        this.id = id;
+    public Transaction(String accountID, float amount) throws InvalidTransaction {
         this.accountID = accountID;
         this.amount = amount;
         isValid();
@@ -18,6 +16,8 @@ public class Transaction {
     public boolean isValid() throws InvalidTransaction {
         if (amount <= 0)
             throw new InvalidTransaction("Amount must be greater than 0");
+        if (amount > 10000)
+            throw new InvalidTransaction("Amount must be less than 10000");
         return true;
     }
 
--- src/tests/TransactionTest.java
+++ src/tests/TransactionTest.java
@@ -13,6 +13,11 @@ public class TransactionTest {
      */
     @Test(expected = InvalidTransaction.class)
     public void should_throw_if_the_amount_is_less_than_1() throws InvalidTransaction {
-        Transaction transaction = new Transaction("id", "AccountID", 0);
+        Transaction transaction = new Transaction("AccountID", 0);
+    }
+
+    @Test(expected = InvalidTransaction.class)
+    public void should_throw_if_the_amount_is_greater_than_10000() throws InvalidTransaction {
+        Transaction transaction = new Transaction("AccountID", 10001);
     }
 }
